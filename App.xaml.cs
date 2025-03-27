624e721db3b9e0565738c00ba0089bfe63934d33
Chill-Astro <MasterArc3435@outlook.com>
2025-03-27 17:39:34 +0530

    Update

--- App.xaml.cs
+++ App.xaml.cs
@@ -16,14 +16,8 @@ using Windows.UI.Core;
 
 namespace Calculator;
 
-// To learn more about WinUI 3, see https://docs.microsoft.com/windows/apps/winui/winui3/.
 public partial class App : Application
 {
-    // The .NET Generic Host provides dependency injection, configuration, logging, and other services.
-    // https://docs.microsoft.com/dotnet/core/extensions/generic-host
-    // https://docs.microsoft.com/dotnet/core/extensions/dependency-injection
-    // https://docs.microsoft.com/dotnet/core/extensions/configuration
-    // https://docs.microsoft.com/dotnet/core/extensions/logging
     public IHost Host
     {
         get;
